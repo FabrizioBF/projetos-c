d2c747240722ab885e422b5ab09322f20cbaa266
Fabrízio Farias <fabrizio.b.f@gmail.com>
2024-02-23 11:39:31 -0300

    exercício4 adicionado

---
+++ exercicio4.c
@@ -0,0 +1,19 @@
+/*
+Este programa exibe o valor do meu salário.
+
+por <digite o seu nome aqui>
+
+criado em <insira a data de criação do arquivo aqui no formato dia/mês/ano>
+**/
+
+#include <stdio.h>
+
+int main(){
+
+int salario; // Declare uma variável chamada salário
+salario = 10000; // Armazenar 10000 no salário
+
+printf("Meu salário é %d.\n", salario);
+return 0;
+
+}
